70ec90c43c63cbe679d2e938f4b8f99574b69dde
guomin <yushi5344@gmail.com>
2018-03-10 13:32:02 +0800

    第四十九节  DOM基础(1)

---
+++ DOM_49/index.js
@@ -0,0 +1,24 @@
+window.onload=function () {
+/**
+ * 节点分为三类
+ * 1.元素节点
+ * 2.文本节点
+ * 3.属性节点
+ * 查找元素
+ *DOM 操作必须等html文档加载完毕才能获取
+ * 怎样获取
+ * 1.把js文件放在html后面执行
+ * 2.onload事件
+ */
+
+
+
+    var box=document.getElementById('box');
+    // alert(box);
+    // alert(box.innerHTML);//box内容
+    // alert(box.tagName);//DIV
+    // alert(box.id);
+    // alert(box.title);
+    // alert(box.style);
+    // alert(box.className);
+}
