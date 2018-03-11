59acfd9681c40a8d329c4323249a14529f2f3307
guomin <yushi5344@gmail.com>
2018-03-11 09:03:04 +0800

    第六十一节  DOM元素尺寸和位置(下)

---
+++ DOM_size_61/index.js
@@ -0,0 +1,24 @@
+window.onload=function () {
+    /**
+     *
+     var box=document.getElementById('box');
+     console.log(box.style.height);
+     console.log(box.style.width);
+     var style=window.getComputedStyle;
+     console.log(style);
+     console.log(box.clientHeight);
+     console.log(box.clientWidth);
+     console.log(box.clientLeft);
+     console.log(box.clientTop);
+
+
+     */
+
+    var box=document.getElementById('box');
+    console.log(box.scrollHeight);
+    console.log(box.scrollWidth);
+    console.log(box.offsetLeft);
+    console.log(box.clientLeft);
+    console.log(box.scrollTop);
+    console.log(box.scrollHeight);
+}
