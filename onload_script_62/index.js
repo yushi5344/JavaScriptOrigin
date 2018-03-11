3a256ac81b18e20ea9aefede30567a5be78dac1f
guomin <yushi5344@gmail.com>
2018-03-11 09:27:56 +0800

    第六十二节  动态加载脚本和样式

---
+++ onload_script_62/index.js
@@ -0,0 +1,37 @@
+window.onload=function () {
+    // var result = browser();
+    // alert(result.name);
+
+
+}
+/**
+ *
+ *
+ var flag=true;
+ if(flag){
+    var script=document.createElement('script');
+    script.type='text/javascript';
+    script.src='../node_modules/browser-detect/dist/browser-detect.js';
+    document.head.appendChild(script);
+}
+
+
+ if(flag){
+    var script=document.createElement('script');
+    script.type='text/javascript';
+    script.appendChild(document.createTextNode("alert('Lee')"));
+    document.head.appendChild(script);
+}
+
+ */
+var flag=true;
+
+if(flag){
+    var style=document.createElement('style');
+    style.type='text/css';
+    style.appendChild(document.createTextNode('#box{color:green;}'));
+    document.head.appendChild(style);
+}
+
+
+
